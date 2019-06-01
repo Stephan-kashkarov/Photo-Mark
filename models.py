9895d932b2a0e55a11e5811069d884966e920fa6
Stephan-kashkarov <kashkarovWork@gmail.com>
2019-06-01 14:18:52 +1000

    started refactor

--- models.py
+++ models.py
@@ -5,86 +5,12 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-
-def region_extraction(frame, regions):
-    pass
-
-
-class Region_Proposer(keras.models.Model):
-    def __init__(self):
-        super().__init__()
-
-    def call(self, frame):
-        pass
-
-    def train(self, labels=None, images=None):
-        pass
-
-
-class Face_Classifier(keras.models.Model):
-    def __init__(self):
-        super().__init__()
-        self.loss = keras.losses.sparse_categorical_crossentropy
-        self.optimizer = keras.optimizers.Adam
-        self.metrics = {
-            "train": [
-                tf.keras.metrics.Mean(name='train_loss'),
-                tf.keras.metrics.SparseCategoricalAccuracy(
-                    name='train_accuracy'
-                ),
-            ],
-            "test": [
-                tf.keras.metrics.Mean(name='test_loss'),
-                tf.keras.metrics.SparseCategoricalAccuracy(
-                    name='test_accuracy'
-                ),
-            ],
-        }
-        self.layers = [
-            keras.layers.Conv2D(64, 8, activation='relu'),
-            keras.layers.Conv2D(64, 4, activation='sigmoid'),
-            keras.layers.MaxPool2D(pool_size=(4, 4)),
-            keras.layers.Conv2D(16, 4, activation='relu'),
-            keras.layers.Conv2D(16, 2, activation='sigmoid'),
-            keras.layers.AvgPool2D(),
-            keras.layers.Flatten(),
-            keras.layers.Dense(64, activation='relu'),
-            keras.layers.Dense(32, activation='sigmoid'),
-            keras.layers.Dense(8, activation='sigmoid'),
-            keras.layers.Dense(2, activation='softmax'),
-        ]
-
-    def call(self, face):
-        for layer in self.layers:
-            face = layer(face)
-        return face
-
-    def train(self, labels=None, images=None):
-        with tf.GradientTape() as tape:
-            predictions = self(images)
-            loss = self.loss(labels, predictions)
-        gradients = tape.gradient(loss, self.trainable_values)
-        self.optimizer.apply_gradients(zip(gradients, self.trainable_values))
-
-
 class Faster_RCNN(keras.models.Model):
     def __init__(self):
-        super().__init__()
-        self.region_proposer = Region_Proposer()
-        self.face_classifier = Face_Classifier()
-        
+        pass
+    
+    def call(self, labels=None, data=None):
+        pass
 
-    def call(self, frame):
-        regions = self.region_proposer.predict(frame)
-        for index, region in region_extraction(frame, regions):
-            if self.face_classifier.predict(region, batch_size=1)[0]:
-                regions.pop(index)
-        return regions
-
-    def train(self, labels=None, images=None)
-        self.region_proposer.train(labels, images)
-        face_data = self.region_proposer(images)
-        for index, image in enumerate(images):
-            pass
-            # Find regions outside images
-        self.face_classifier.train()
+    def train(self, labels=None, data=None):
+        pass
