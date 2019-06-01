04f360b0b2e4fe3eda9b30082a16a38d47319589
Stephan-kashkarov <kashkarovWork@gmail.com>
2019-06-01 19:21:15 +1000

    added rpn layers

--- models.py
+++ models.py
@@ -6,8 +6,27 @@ import pandas as pd
 import tensorflow as tf
 
 class RPN(keras.models.Model):
-    def __init__(self):
-        pass
+    def __init__(self, **kwargs):
+        self.anchors = kwargs.get('anchors', 9)
+        self.base = keras.layers.Conv2D(
+            256,
+            (3, 3),
+            padding='same',
+            activation='relu',
+            kernel_initializer='normal'
+        )
+        self.classifier = keras.layers.Conv2D(
+            self.anchors,
+            (1, 1),
+            activation='sigmoid',
+            kernel_initializer='uniform',
+        )
+        self.regressor = keras.layers.Conv2D(
+            self.anchors * 4,
+            (1, 1),
+            activation='linear',
+            kernel_initializer='zero',
+        )
 
     def call(self, data):
         pass
@@ -20,7 +39,7 @@ class Faster_RCNN(keras.models.Model):
     def __init__(self):
         self.optimizer = keras.optimizers.Adam
 
-        self.vgg16 = [
+        self.vgg16 = keras.models.Sequential([
             keras.layers.ZeroPadding2D((1,1),input_shape=(3,224,224)),
             keras.layers.Convolution2D(64, 3, 3, activation='relu'),
             keras.layers.ZeroPadding2D((1,1)),
@@ -56,7 +75,7 @@ class Faster_RCNN(keras.models.Model):
             keras.layers.ZeroPadding2D((1,1)),
             keras.layers.Convolution2D(512, 3, 3, activation='relu'),
             keras.layers.MaxPooling2D((2,2), strides=(2,2)),
-        ]
+        ])
 
         self.rpn = RPN()
     
