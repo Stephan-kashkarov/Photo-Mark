689b4c9d601fb44d78c8d651dc5da0d8f7c4b71f
Stephan-kashkarov <kashkarovWork@gmail.com>
2019-06-03 20:41:57 +1000

    added docstrings

--- models.py
+++ models.py
@@ -4,6 +4,33 @@ import pandas as pd
 import tensorflow as tf
 
 class RPN(keras.models.Model):
+    """
+    ######################################
+    #### Region Propositional Network ####
+    ######################################
+
+    This network is designed to take the last convolutional
+    layer from a VGG16 pretrained network. 
+    
+    The network then iterates through every point in this
+    image and creates n bounding boxes named anchors. 
+    N is defined through the Kwarg "anchors" and defaults to 9. 
+    Each of these anchors are then scored by "objectivness".
+    In other words, the chance the object is background or an object. 
+    Given the objectiveness of said anchor is higher then the threshold
+    defined in the "objectiveness_limit" kwarg, then this
+    anchor is selected to have its bounding box regressed.
+    All matching bounding boxes are then given as output.
+
+    Keyword Arguements:
+        -> anchors            -- (default, 9)   | Number of anchors per point               
+        -> objectivness_limit -- (default, 0.7) | objetivness threshold for selected object 
+
+    Methods:
+        -> call    -- Binding to object call method
+        -> train   -- Training method, used to train network
+        -> anchors -- used to generate anchors given coord
+    """
     def __init__(self, **kwargs):
         self.num_anchors = kwargs.get('anchors', 9)
         self.objectness_limit = kwargs.get('objectness_limit', 0.7)
@@ -34,7 +61,6 @@ class RPN(keras.models.Model):
                 if self.classifier(anchor) > self.objectness_limit:
                     yield self.regressor(coords)
 
-        
 
     def train(self, labels, data):
         pass
@@ -43,6 +69,27 @@ class RPN(keras.models.Model):
         pass
 
 class Detector(keras.models.Model):
+    """
+    ##################
+    #### Detector ####
+    ##################
+
+    This network is designed to take the last convolutional
+    layer from a VGG16 pretrained network and a Region Propositinal
+    Network. 
+
+    This network is designed to take coordinates from an RPN and
+    classify the content in the given coordinates upon a convolutional
+    layer given by a VGG16 network. This is where the bulk of the
+    Faster R-CNN's computational power is used.
+
+    Keyword Arguments:
+        ->
+
+    Methods:
+        -> call    -- Binding to object call method
+        -> train   -- Training method, used to train network
+    """
     def __init__(self):
         pass
 
@@ -53,6 +100,11 @@ class Detector(keras.models.Model):
         pass
 
 class Faster_RCNN(keras.models.Model):
+    """
+    ######################################################
+    #### Faster Regional Convolutional Neural Network ####
+    ######################################################
+    """
     def __init__(self):
         self.optimizer = keras.optimizers.Adam
 
