9aa361b7a93d90b1a5ed5ae94367f54d2f63271c
Stephan-kashkarov <kashkarovwork@gmail.com>
2019-06-10 20:34:18 +1000

    worked on commenting

--- losses.py
+++ losses.py
@@ -2,6 +2,15 @@ import keras
 import numpy as np
 
 def smooth_l1_loss(label, pred, anchor):
+    """
+    Smooth L1 loss for bbox regression
+
+    This loss calculates how off the anchors are
+    from the correct anchor.
+
+    It is used to calculate new weights for the 
+    network.
+    """
     label[0] = (label[0] - anchor[0])/anchor[2]
     label[1] = (label[1] - anchor[1])/anchor[3]
     label[2] = np.log10(label[2]/anchor[2])
--- models.py
+++ models.py
@@ -1,6 +1,14 @@
+""" Models.py
+
+classes:
+-> RPN : line 15
+-> Classifier : line 145
+-> Faster_RCNN : 225
+
+"""
+
 import keras
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 
 from losses import smooth_l1_loss
@@ -24,7 +32,8 @@ class RPN(keras.models.Model):
     All matching bounding boxes are then given as output.
 
     Keyword Arguements:        
-        -> objectness_limit -- (default, 0.7) | objetivness threshold for selected object 
+        -> objectness_limit -- (default, 0.7) | objetivness threshold 
+                                              | for selected object 
 
     Methods:
         -> call    -- Binding to object call method
@@ -35,7 +44,7 @@ class RPN(keras.models.Model):
         self.objectness_limit = kwargs.get('objectness_limit', 0.7)
         self.const = kwargs.get('loss_const', 1)
 
-        self.optimiser = keras.optimizers.Adam()
+        self.optimiser = tf.train.AdamOptimizer()
 
         self.base = keras.layers.Conv2D(
             256,
@@ -45,6 +54,8 @@ class RPN(keras.models.Model):
             kernel_initializer='normal'
         )
 
+        # TODO: figure out how the reg and cls layers work
+        # with variable sized inputs
         self.classifier = keras.layers.Conv2D(
             9,
             (1, 1),
@@ -59,14 +70,28 @@ class RPN(keras.models.Model):
         )
 
     def call(self, data, training=False):
+        """
+        Base class func Call
+
+        method for calling network
+
+        args:
+            -> Data | System input | dtype: tf.Tensor
+            -> training | variable output bool | dtype: bool
+        
+        """
         data = self.base(data)
 
         # sliding window predictor
         for anchor in self.anchors(np.ndindex(data)):
             objectness = self.classifier(anchor, data)
+
+            # objectness threshold
             if objectness > self.objectness_limit:
                 regressed_box = self.regressor(anchor, data)
-                if regressed_box[2] > 10 and regressed_box[3] > 10:
+
+                # min size boxes
+                if regressed_box[2] > 10 and regressed_box[3] > 10: # size limit
                     if not training:
                         yield regressed_box, objectness
                     else:
@@ -75,29 +100,53 @@ class RPN(keras.models.Model):
                 if not training:
                     yield None, objectness
                 else:
+                    # returns anchor for training math
                     yield None, objectness, anchor
 
     def train(self, labels, data):
+        """
+        Base class func Train
+
+        Function called to train layer
+
+        Args:
+            -> labels | correct output | dtype: list
+            -> Data | System input | dtype: tf.Tensor
+        """
         prediction = self(data, training=True)
+
+        # Magic TF bindings for gradient decent
         with tf.GradientTape() as tape:
+
+            # iterate anchors
             for index, pred, anchor in enumerate(prediction):
+
+                # loss calc (see losses.py)
                 cls_loss += keras.losses.binary_crossentropy(labels[index][1], pred[1])
                 reg_loss += smooth_l1_loss(labels[index][0], pred[0], anchor)
             loss = (cls_loss / 256) + (self.const * (reg_loss/(9*256)))
+
+        # calculates gradients of last iteration
         grads = tape.gradient(loss, self.trainable_weights)
+        # applies said graidents (with adam)
         self.optimiser.apply_gradients(zip(grads, self.trainable_weights))
 
 
     def anchors(self, point):
+        """
+        RPN.anchors
+
+        Private method for generating anchors around a single point
+        """
         for aspect in [(2, 1), (1, 1), (1, 2)]:
             for scale in [128, 192, 256]:
                 yield (point[0], point[1], aspect[0]*scale, aspect[1]*scale)
 
-class Detector(keras.models.Model):
+class Classifier(keras.models.Model):
     """
-    ##################
-    #### Detector ####
-    ##################
+    ####################
+    #### Classifier ####
+    ####################
 
     This network is designed to take the last convolutional
     layer from a VGG16 pretrained network and a Region Propositinal
@@ -108,21 +157,70 @@ class Detector(keras.models.Model):
     layer given by a VGG16 network. This is where the bulk of the
     Faster R-CNN's computational power is used.
 
-    Keyword Arguments:
-        ->
+
+    TODO: This classifier currently chooses either person or not
+    person. However as this is a full on classifier which is used
+    sparingly within the network, it could easily be expanded to do
+    much more intensive classification
+
+    Ideas:
+        -> Use this to do facial recognition
+        -> Use this to detect between animals and simmilar things
 
     Methods:
         -> call    -- Binding to object call method
         -> train   -- Training method, used to train network
     """
     def __init__(self):
-        pass
+        self.layers = [
+            # Conv block 1
+            keras.layers.Conv2D(128, (3, 3), activation='relu'),
+            keras.layers.Conv2D(128, (3, 3), activation='relu'),
+            keras.layers.AvgPool2D(2, 2),
+
+            # Conv block 1
+            keras.layers.Conv2D(256, (3, 3), activation='relu'),
+            keras.layers.Conv2D(256, (3, 3), activation='sigmoid'),
+            keras.layers.AvgPool2D(2, 2),
+
+            # Dense layer
+            keras.layers.Flatten(),
+            keras.layers.Dense(256, activation='relu'),
+            keras.layers.Dense(64, activation='sigmoid'),
+            keras.layers.Dense(2, activation='softmax'),
+        ]
 
     def call(self, data):
-        pass
-    
+        """
+        Base class func Call
+
+        method for calling network
+
+        args:
+            -> Data | System input | dtype: tf.Tensor
+        
+        """
+        # TODO: Figure out how to do variable input sizes
+        for layer in self.layers:
+            data = layer(data)
+        return data
+
+
     def train(self, labels, data):
-        pass
+        """
+        Base class func Train
+
+        Function called to train layer
+
+        Args:
+            -> labels | correct output | dtype: list
+            -> Data | System input | dtype: tf.Tensor
+        """
+        with tf.GradientTape() as tape:
+            pred = self(data)
+            loss = self.loss(pred)
+        grad = tape.gradient(loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grad, self.trainable_weights))
 
 class Faster_RCNN(keras.models.Model):
     """
@@ -137,28 +235,84 @@ class Faster_RCNN(keras.models.Model):
     The image is first put through a VGG16 pretrained CNN.
     The final convolution of which is fed through the RPN
     which returns a serise of bounding boxes. Each of these
-    boxes is then classified by the detector network.
-
-    Keyword Arguments:
-        ->
+    boxes is then classified by the classifier network.
 
     Methods:
         -> call    -- Binding to object call method
         -> train   -- Training method, used to train network
     """
     def __init__(self):
-        self.optimizer = keras.optimizers.Adam
+        self.optimizer = tf.train.AdamOptimizer()
 
         self.vgg16 = keras.applications.vgg16.VGG16(weights='imagenet', include_top=False)
 
         self.rpn = RPN()
-        self.detector = Detector()
+        self.classifier = Classifier()
     
     def call(self, data):
+        """
+        Base class func Call
+
+        method for calling network
+
+        args:
+            -> Data | System input | dtype: tf.Tensor
+        """
+        # runs vgg 16
         data = self.vgg16(data)
+
+        # predicts regions
         regions = self.rpn(data)
+
+        # runs regions through classifier
         for region in regions:
-            yield region, self.detector(region, data)
+            pred = self.classifier(region)
+            if np.sum(pred) > 0:
+                yield region, pred
 
     def train(self, labels, data):
-        pass
+        """
+        Base class func Train
+
+        Function called to train layer
+
+        This method has a unique training method,
+        Anchors and scores are calculated by the rpn.
+        The Classifier is then run on these anchors
+        using the inverse losses from the scores as
+        their respective labels. 
+
+        This method allows for both networks to be
+        trained at the same time. The classifier trains
+        better the better then RPN performs however.
+
+        Args:
+            -> labels | correct output | dtype: list
+            -> Data | System input | dtype: tf.Tensor
+        """
+
+        with tf.GradientTape() as tape:
+            pred = self.rpn(data)
+            # iterate anchors
+            for index, pred, anchor in enumerate(pred):
+
+                # rpn loss part 1
+                _cls = keras.losses.binary_crossentropy(labels[index][1], pred[1])
+                cls_loss += _cls
+
+                # train the Classifier
+                with tf.GraidentTape() as tape2:
+                    pred2 = self.classifier(anchor)
+                    class_loss = keras.losses.binary_crossentropy((1 - _cls), pred2)
+                
+                # Classifier grad decsent
+                grad = tape.gradient(class_loss, self.classifier.trainable_weights)
+                self.classifier.optimizer.apply_gradients(zip(grad, self.classifier.trainable_weights))
+
+                # RPN loss prt 2 the reckoning
+                reg_loss += smooth_l1_loss(labels[index][0], pred[0], anchor)
+            rpn_loss = (cls_loss / 256) + (self.rpn.const * (reg_loss/(9*256)))
+
+            # RPN grad decsent
+            grad = tape.gradient(rpn_loss, self.rpn.trainable_weights)
+            self.rpn.optimizer.apply_gradients(zip(grad, self.rpn.trainable_weights))
